016bddd55ac43553dca19d83f7d090eaf9e6b6d2
kyunmin77 <geonmin77@gmail.com>
2024-04-28 01:36:07 +0900

    Iterator을 이용하여 출력명령 통일

---
+++ LearningJava/src/Collection_Framework/IteratorEx1.java
@@ -0,0 +1,23 @@
+package Collection_Framework;
+
+import java.util.*;
+
+public class IteratorEx1 {
+
+	public static void main(String[] args) {
+		ArrayList list = new ArrayList();
+		list.add("1");
+		list.add("2");
+		list.add("3");
+		list.add("4");
+		list.add("5");
+
+		Iterator it = list.iterator();
+		
+		while(it.hasNext()) {
+			Object o = it.next();
+			System.out.println(o);
+		}
+	}
+
+}
